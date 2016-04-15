6f4967d7233f540b18c20073352a7db2aa9d4a8c
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-04-15 11:28:01 +0700

    basic sms inbox, outbox, send sms done

--- app/src/main/java/id/ac/itb/securesms/InboxActivity.java
+++ app/src/main/java/id/ac/itb/securesms/InboxActivity.java
@@ -1,5 +1,10 @@
 package id.ac.itb.securesms;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +17,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class InboxActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener {
+
+    private final String INBOX = "inbox";
+    private final String OUTBOX = "sent";
+    private static InboxActivity inst;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView smsListView;
+    ArrayAdapter arrayAdapter;
+
+    public void refreshSmsList(String option) {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsListCursor = contentResolver.query(Uri.parse("content://sms/"+option), null, null, null, null);
+        int indexBody = smsListCursor.getColumnIndex("body");
+        int indexAddress = smsListCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsListCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = "SMS From: " + smsListCursor.getString(indexAddress) +
+                    "\n" + smsListCursor.getString(indexBody) + "\n";
+            arrayAdapter.add(str);
+        } while (smsListCursor.moveToNext());
+    }
+
+    public void updateList(final String smsMessage) {
+        arrayAdapter.insert(smsMessage, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+    public static InboxActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +74,9 @@ public class InboxActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Context context = view.getContext();
+                Intent intent = new Intent(context, NewMessageActivity.class);
+                context.startActivity(intent);
             }
         });
 
@@ -40,6 +88,12 @@ public class InboxActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        smsListView = (ListView) findViewById(R.id.SMSList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        smsListView.setAdapter(arrayAdapter);
+        smsListView.setOnItemClickListener(this);
+
+        refreshSmsList(INBOX);
     }
 
     @Override
@@ -80,22 +134,36 @@ public class InboxActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_inbox) {
+            refreshSmsList(INBOX);
+        } else if (id == R.id.nav_outbox) {
+            refreshSmsList(OUTBOX);
+        } else if (id == R.id.nav_signature) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_encryption) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        try {
+            String[] smsMessages = smsMessagesList.get(position).split("\n");
+            String address = smsMessages[0];
+            String smsMessage = "";
+            for (int i = 1; i < smsMessages.length; ++i) {
+                smsMessage += smsMessages[i];
+            }
+
+            String smsMessageStr = address + "\n";
+            smsMessageStr += smsMessage;
+            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/id/ac/itb/securesms/NewMessageActivity.java
@@ -0,0 +1,70 @@
+package id.ac.itb.securesms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class NewMessageActivity extends AppCompatActivity {
+
+    private CheckBox encryptCheck;
+    private TextView msgLength;
+    private EditText recepientText, messageText, keyText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        encryptCheck = (CheckBox) findViewById(R.id.encryptCheckBox);
+        msgLength = (TextView) findViewById(R.id.msgLength);
+        recepientText = (EditText) findViewById(R.id.recepientText);
+        messageText = (EditText) findViewById(R.id.messageText);
+        messageText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                msgLength.setText(String.valueOf(s.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        keyText = (EditText) findViewById(R.id.keyText);
+        sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager sms = SmsManager.getDefault();
+                ArrayList<String> parts = sms.divideMessage(messageText.getText().toString());
+                sms.sendMultipartTextMessage(recepientText.getText().toString(), null, parts, null, null);
+                Log.d("SMS", "sending message to " + recepientText.getText().toString() + " " + messageText.getText().toString());
+                Toast.makeText(v.getContext(), "sending message", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}

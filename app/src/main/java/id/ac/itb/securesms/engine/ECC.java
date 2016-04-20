7b1875eb25d4253bcdee6ed03a9544fa4b9715c9
Andre Susanto <andre@andresusanto.info>
2016-04-21 00:39:17 +0700

    Move ECC from KriptoSuite

--- app/src/main/java/id/ac/itb/securesms/engine/ECC.java
+++ app/src/main/java/id/ac/itb/securesms/engine/ECC.java
@@ -1,41 +1,126 @@
 package id.ac.itb.securesms.engine;
 
-import java.math.BigInteger;
-import java.util.Random;
-
 import id.ac.itb.securesms.obj.Coordinate;
 import id.ac.itb.securesms.obj.Curve;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Random;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  *
  * @author Andre
  */
 public class ECC {
-
+    private MessageDigest hash;
     private Curve curve;
     private Coordinate baseCoordinate;
     private BigInteger privateKey;
 
 
-    public ECC (Curve curve, Coordinate base, BigInteger privateKey){
+    public ECC (Curve curve, Coordinate base, BigInteger privateKey) throws NoSuchAlgorithmException{
         this.curve = curve;
         this.baseCoordinate = base;
         this.privateKey = privateKey;
+        hash = MessageDigest.getInstance("SHA-1");
     }
 
     public Coordinate generatePublic(){
         return multiplyCoordinate(baseCoordinate, privateKey);
     }
 
-    // fungsi untuk melakukan enkripsi data byte
-    public byte[] encrypt(byte[] input, Coordinate publicKey){
-        byte[] encryptedFile = new byte[2 * input.length];
+    // fungsi untuk memverifikasi tandatangan digital
+    public boolean verify(byte[] message, byte[] signature, Coordinate publicKey){
+        BigInteger r = new BigInteger(Arrays.copyOfRange(signature, 0, signature.length/2));
+        BigInteger s = new BigInteger(Arrays.copyOfRange(signature, signature.length/2, signature.length));
+        BigInteger e = calculateE(curve.n, message);
 
-        for(int i=0; i< input.length; i++) {
-            Coordinate plain = new Coordinate(input[i], curve);
-            Coordinate[] encrypted = encrypt(plain, publicKey);
-            encryptedFile[i*2] = encrypted[0].toByte();
-            encryptedFile[i*2+1] = encrypted[1].toByte();
+        System.out.println("R' : " + r.toString());
+        System.out.println("S' : " + s.toString());
+
+        if (r.compareTo(BigInteger.ONE) < 0 || r.compareTo(curve.n) >= 0) return false; // r berada di [1, p - 1]
+        if (s.compareTo(BigInteger.ONE) < 0 || s.compareTo(curve.n) >= 0) return false; // s berada di [1, p - 1]
+
+        System.out.println("OKE");
+
+        BigInteger w = s.modInverse(curve.n);
+
+        BigInteger u1 = e.multiply(w).mod(curve.n);
+        BigInteger u2 = r.multiply(w).mod(curve.n);
+
+        Coordinate curvePoint = addCoordinate( multiplyCoordinate(this.baseCoordinate, u1), multiplyCoordinate(publicKey, u2) );
+
+        System.out.println("X : " + curvePoint.X.mod(curve.n).toString());
+
+        return r.equals(curvePoint.X.mod(curve.n));
+    }
+
+    // fungsi untuk menghasilkan digital signature
+    // ukuran signature yang dihasilkan adalah 2 x ukuran P (dalam byte)
+    public byte[] sign(byte[] input) throws IOException{
+        BigInteger e = calculateE(curve.n, input);
+
+        BigInteger r, s;
+        byte[] rb, sb;
+
+        do { // generate s
+            BigInteger k = new BigInteger(curve.n.bitLength(), new Random());
+
+            do // generate r
+            {
+                Coordinate p = multiplyCoordinate(baseCoordinate, k);
+                r = p.X.mod(curve.n);
+
+            } while (r.equals(BigInteger.ZERO));
+
+            s = k.modInverse(curve.n).multiply(e.add(this.privateKey.multiply(r))).mod(curve.n);
+
+            rb = r.toByteArray();
+            sb = s.toByteArray();
+
+            // padding bit jika panjangnya < panjang p
+            if (rb.length != curve.pLength){
+                byte tmp[] = new byte[curve.pLength];
+                System.arraycopy(rb, 0, tmp, curve.pLength - rb.length, rb.length);
+                rb = tmp;
+            }
+
+            if (sb.length != curve.pLength){
+                byte tmp[] = new byte[curve.pLength];
+                System.arraycopy(sb, 0, tmp, curve.pLength - sb.length, sb.length);
+                sb = tmp;
+            }
+        }while (s.equals(BigInteger.ZERO));
+
+        byte result[] = new byte[rb.length * 2];
+        System.arraycopy(rb, 0, result, 0, rb.length);
+        System.arraycopy(sb, 0, result, rb.length, rb.length);
+
+        return result;
+    }
+
+    // fungsi untuk melakukan enkripsi data byte
+    public byte[] encrypt(byte[] input, Coordinate publicKey) throws IOException{
+        if (curve.pLength != input.length) throw new IOException("Input size not match!");
+
+        byte[] encryptedFile = new byte[(curve.pLength + 1) * 2];
+        BigInteger k = new BigInteger(curve.p.bitLength(), new Random());
+
+        Coordinate gamma = multiplyCoordinate(this.baseCoordinate, k);
+        Coordinate sec = multiplyCoordinate(publicKey, k);
+        System.arraycopy(gamma.toByte(curve), 0, encryptedFile, 0, curve.pLength + 1);
+
+        hash.reset();
+        hash.update(sec.X.toByteArray());
+        hash.update(sec.Y.toByteArray());
+
+        byte[] digest = hash.digest();
+
+        // isi data ke byte
+        for(int j = 0; j < input.length; j++) {
+            encryptedFile[ j + curve.pLength + 1 ] = (byte)(input[j]^digest[j]);
         }
 
         return encryptedFile;
@@ -54,15 +139,22 @@ public class ECC {
         return ans; // kembalikan tupel
     }
 
-    public byte[] decrypt(byte[] input){
-        byte[] decryptedFile = new byte[input.length / 2];
+    public byte[] decrypt(byte[] input) throws IOException{
+        if (curve.pLength != (input.length / 2) - 1) throw new IOException("Input size not match!");
+        byte[] decryptedFile = new byte[curve.pLength];
+        byte[] gammacom = new byte[curve.pLength + 1];
 
-        for(int i = 0; i < input.length; i += 2) {
-            Coordinate[] encrypted = new Coordinate[2];
-            encrypted[0] = new Coordinate(input[i], curve);
-            encrypted[1] = new Coordinate(input[i + 1], curve);
-            Coordinate decrypted = decrypt(encrypted);
-            decryptedFile[i / 2] = decrypted.toByte();
+        System.arraycopy(input, 0, gammacom, 0, curve.pLength + 1);
+        Coordinate gamma = new Coordinate(gammacom, curve);
+        Coordinate sec = multiplyCoordinate(gamma, this.privateKey);
+
+        hash.reset();
+        hash.update(sec.X.toByteArray());
+        hash.update(sec.Y.toByteArray());
+
+        byte[] digest = hash.digest();
+        for(int j = 0; j < decryptedFile.length; j++) {
+            decryptedFile[j] = (byte)(input[j + curve.pLength + 1] ^ digest[j]);
         }
 
         return decryptedFile;
@@ -73,37 +165,45 @@ public class ECC {
         return subtractCoordinate(cipher[1], multiWithPrivate);
     }
 
+    // fungsi untuk menghitung nilai E untuk keperluan ECC DSA
+    private BigInteger calculateE(BigInteger n, byte[] message){
+        int log2n = n.bitLength();
+        int messageBitLength = message.length * 8;
+
+        BigInteger e = new BigInteger(1, message);
+        if (log2n < messageBitLength){
+            e = e.shiftRight(messageBitLength - log2n);
+        }
+
+        return e;
+    }
+
     // operasi penambahan titik dalam kurva
     public Coordinate addCoordinate(Coordinate a, Coordinate b) {
-        if (a.isEqual(b)){
-            // Hitung lamda untuk titik yang sama
-            BigInteger three = new BigInteger("3");
-            BigInteger two = new BigInteger("2");
-            BigInteger temp = new BigInteger(a.X.toString());
+        if (a.isZero()) return new Coordinate(b);
+        else if (b.isZero()) return new Coordinate(a);
+        BigInteger lambda = null;
+        BigInteger three = new BigInteger("3");
+        BigInteger two = new BigInteger("2");
 
-            BigInteger lambda = temp.modPow(two, curve.p);
-            lambda = three.multiply(lambda);
-            lambda = lambda.add(curve.a);
-            BigInteger den = two.multiply(a.Y);
-            lambda = lambda.multiply(den.modInverse(curve.p));
+        if (a.X.equals(b.X)){
+            if (a.Y.equals(b.Y)){
+                lambda = ((a.X.modPow(two,curve.p)).multiply(three)).add(curve.a);
+                lambda = (lambda.multiply((two.multiply(a.Y)).modInverse(curve.p))).mod(curve.p);
+            }else{
+                return new Coordinate(BigInteger.ZERO, BigInteger.ZERO);
+            }
 
-            // Hitung nilai X dan Y yang baru
-            BigInteger newX = lambda.multiply(lambda).subtract(a.X).subtract(a.X).mod(curve.p);
-            BigInteger newY = (lambda.multiply(a.X.subtract(newX))).subtract(a.Y).mod(curve.p);
-            return new Coordinate(newX, newY);
-        }else if (isCoordinateMirror(a, b)){
-            return new Coordinate(BigInteger.ZERO, BigInteger.ZERO);
         }else{
-            // Hitung lambda untuk titik berbeda
-            BigInteger lambda = b.Y.subtract(a.Y);
-            BigInteger den = b.X.subtract(a.X);
-            lambda = lambda.multiply(den.modInverse(curve.p));
-
-            // Hitung nilai X dan Y yang baru
-            BigInteger newX = lambda.multiply(lambda).subtract(a.X).subtract(b.X).mod(curve.p);
-            BigInteger newY = (lambda.multiply(a.X.subtract(newX))).subtract(a.Y).mod(curve.p);
-            return new Coordinate(newX, newY);
+            lambda = ((b.Y.subtract(a.Y)).multiply((b.X.subtract(a.X)).modInverse(curve.p))).mod(curve.p);
         }
+
+        // Hitung nilai X dan Y yang baru
+        BigInteger newX = (((lambda.modPow(two,curve.p)).subtract(b.X)).subtract(a.X)).mod(curve.p);
+        BigInteger newY = ((lambda.multiply(a.X.subtract(newX))).subtract(a.Y)).mod(curve.p);
+
+        //return new Coordinate(newX, newY);
+        return new Coordinate(newX, newY, curve);
     }
 
     public boolean isCoordinateMirror(Coordinate a, Coordinate b){
--- app/src/main/java/id/ac/itb/securesms/obj/Curve.java
+++ app/src/main/java/id/ac/itb/securesms/obj/Curve.java
@@ -6,23 +6,37 @@ import java.math.BigInteger;
  *
  * @author Andre
  */
+
 public class Curve {
 
     // rumus dari kurva eliptik adalah: y^2 = x^3 + ax + b mod p, dimana P = bilangan prima
     public BigInteger a;
     public BigInteger b;
     public BigInteger p;
+    public BigInteger n;
 
-    public Curve(BigInteger a, BigInteger b, BigInteger p){
+    public byte[] pb;
+    public int pLength;
+
+    public Curve(BigInteger a, BigInteger b, BigInteger p, BigInteger n){
         this.a = a;
         this.b = b;
         this.p = p;
+        this.n = n;
+
+        calculatePByte();
     }
 
     public Curve(Curve other){
         this.a = new BigInteger(other.a.toString());
         this.b = new BigInteger(other.b.toString());
         this.p = new BigInteger(other.p.toString());
+
+        calculatePByte();
     }
 
+    public void calculatePByte(){
+        this.pb = p.toByteArray();
+        this.pLength = this.pb.length;
+    }
 }

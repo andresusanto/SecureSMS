a5240fa272da22d4f16a2d578421c43cb0157e04
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-04-24 14:53:47 +0700

    generate ECC keys

--- app/src/main/java/id/ac/itb/securesms/app/SignatureActivity.java
+++ app/src/main/java/id/ac/itb/securesms/app/SignatureActivity.java
@@ -1,5 +1,7 @@
 package id.ac.itb.securesms.app;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.Random;
 
 import id.ac.itb.securesms.R;
 import id.ac.itb.securesms.engine.ECC;
@@ -27,19 +32,42 @@ public class SignatureActivity extends AppCompatActivity {
         toolbar.setTitle("Settings");
         setSupportActionBar(toolbar);
 
+        SharedPreferences sp = getSharedPreferences("ECC_Key", Activity.MODE_PRIVATE);
         ecc = new ECC();
         publicKey = ecc.generatePublic();
+        String publicStr = sp.getString("public",publicKey.X.toString() + "," + publicKey.Y.toString());
+        String privateStr = sp.getString("private",ecc.getPrivateKey().toString());
         publicKeyText = (TextView) findViewById(R.id.publicKeyView);
-        publicKeyText.setText(publicKey.X.toString() + "," + publicKey.Y.toString());
+        publicKeyText.setText(publicStr);
         privateKeyText = (TextView) findViewById(R.id.privateKeyView);
-        privateKeyText.setText(ecc.getPrivateKey().toString());
+        privateKeyText.setText(privateStr);
         generateKeyButton = (Button) findViewById(R.id.generateKeyButton);
         generateKeyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // PEMBANGKITAN PUBLIC KEY + PRIVATE KEY
+                StringBuilder sb = new StringBuilder();
+                Random random = new Random();
+                BigInteger newPrivate;
+
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(999999));
+                sb.append(random.nextInt(9999));
+                newPrivate = new BigInteger(sb.toString());
+                ecc.setPrivateKey(newPrivate);
+                privateKeyText.setText(newPrivate.toString());
                 publicKey = ecc.generatePublic();
                 publicKeyText.setText(publicKey.X.toString() + "," + publicKey.Y.toString());
+                SharedPreferences sp = getSharedPreferences("ECC_Key", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("public", publicKeyText.getText().toString());
+                editor.putString("private", privateKeyText.getText().toString());
+                editor.commit();
             }
         });
     }
